e9fe140cc465d3000cee2198577374cd75441784
joelis10 <joelnod64@gmail.com>
2022-10-28 15:29:02 +0100

    added storing accessibility choices (needs work)

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
+window.onload = function () {
+  const checkAnim = localStorage.getItem("animation");
+  const checkShadow = localStorage.getItem("shadow");
+  const checkSimpleText = localStorage.getItem("simpleText");
+
+  if (checkAnim == "off") {
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.add("noAnim"));
+  } else {
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.remove("noAnim"));
+  }
+
+  if (checkShadow == "off") {
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.add("noShadow"));
+  } else {
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.remove("noShadow"));
+  }
+
+  if (checkSimpleText == "true") {
+    document.querySelector(".title").classList.add("simple");
+  } else if(checkSimpleText == "false") {
+    document.querySelector(".title").classList.remove("simple");
+  }
+};
+
 document.querySelector(".Anim").addEventListener("click", function (event) {
   event.target.classList.toggle("toggled");
   document
     .querySelectorAll(".project")
     .forEach((item) => item.classList.toggle("noAnim"));
+
+  if (event.target.classList.contains("toggled")) {
+    localStorage.setItem("animation", "off");
+  } else {
+    localStorage.setItem("animation", "on");
+  }
 });
 
 document.querySelector(".Shadow").addEventListener("click", function (event) {
@@ -10,6 +48,12 @@ document.querySelector(".Shadow").addEventListener("click", function (event) {
   document
     .querySelectorAll(".project")
     .forEach((item) => item.classList.toggle("noShadow"));
+
+  if (event.target.classList.contains("toggled")) {
+    localStorage.setItem("shadow", "off");
+  } else {
+    localStorage.setItem("shadow", "on");
+  }
 });
 
 document
@@ -17,6 +61,12 @@ document
   .addEventListener("click", function (event) {
     event.target.classList.toggle("toggled");
     document.querySelector(".title").classList.toggle("simple");
+
+    if (event.target.classList.contains("toggled")) {
+      localStorage.setItem("simpleText", "false");
+    } else {
+      localStorage.setItem("simpleText", "true");
+    }
   });
 
 /*
