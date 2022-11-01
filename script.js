edd797d751736f5f3d432d8f272d3cd8a8f5cb32
joelis10 <joelnod64@gmail.com>
2022-11-01 15:14:16 +0000

    added comments to homepage using Disqus

--- script.js
+++ script.js
@@ -2,41 +2,56 @@ window.onload = function () {
   const checkAnim = localStorage.getItem("animation");
   const checkShadow = localStorage.getItem("shadow");
   const checkSimpleText = localStorage.getItem("simpleText");
-  const checkIfDyslexic = localStorage.getItem("dyslexic")
+  const checkIfDyslexic = localStorage.getItem("dyslexic");
 
   if (checkAnim == "off") {
-    document.querySelectorAll(".project").forEach((item) => item.classList.add("noAnim"));
-    document.querySelector(".Anim").classList.add("toggled")
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.add("noAnim"));
+    document.querySelector(".Anim").classList.add("toggled");
   } else {
-    document.querySelectorAll(".project").forEach((item) => item.classList.remove("noAnim"));
-    document.querySelector(".Anim").classList.remove("toggled")
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.remove("noAnim"));
+    document.querySelector(".Anim").classList.remove("toggled");
   }
 
   if (checkShadow == "off") {
-    document.querySelectorAll(".project").forEach((item) => item.classList.add("noShadow"));
-    document.querySelector(".Shadow").classList.add("toggled")
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.add("noShadow"));
+    document.querySelector(".Shadow").classList.add("toggled");
   } else {
-    document.querySelectorAll(".project").forEach((item) => item.classList.remove("noShadow"));
-    document.querySelector(".Shadow").classList.remove("toggled")
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.remove("noShadow"));
+    document.querySelector(".Shadow").classList.remove("toggled");
   }
 
   if (checkSimpleText == "true") {
     document.querySelector(".title").classList.add("simple");
-    document.querySelector(".simpleText").classList.remove("toggled")
+    document.querySelector(".simpleText").classList.remove("toggled");
   } else if (checkSimpleText == "false") {
     document.querySelector(".title").classList.remove("simple");
-    document.querySelector(".simpleText").classList.add("toggled")
+    document.querySelector(".simpleText").classList.add("toggled");
   }
 
   if (checkIfDyslexic == "true") {
     document.querySelector(".title").classList.add("dyslexic");
-    document.querySelectorAll(".project").forEach(item => item.classList.add("dyslexic"));
-    document.querySelector(".dyslexiaText").classList.remove("toggled")
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.add("dyslexic"));
+    document.querySelector(".dyslexiaText").classList.remove("toggled");
   } else if (checkSimpleText == "false") {
     document.querySelector(".title").classList.remove("simple");
-    document.querySelectorAll(".project").forEach(item => item.classList.remove("dyslexic"))
-    document.querySelector(".dyslexiaText").classList.add("toggled")
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.remove("dyslexic"));
+    document.querySelector(".dyslexiaText").classList.add("toggled");
   }
+
+  document.getElementById('allComments').innerHTML = localStorage.getItem('template');
+
 };
 
 document.querySelector(".Anim").addEventListener("click", function (event) {
@@ -65,9 +80,12 @@ document.querySelector(".Shadow").addEventListener("click", function (event) {
   }
 });
 
-document.querySelector(".simpleText").addEventListener("click", function (event) {
+document
+  .querySelector(".simpleText")
+  .addEventListener("click", function (event) {
     event.target.classList.toggle("toggled");
     document.querySelector(".title").classList.toggle("simple");
+    document.querySelector(".leaveComment").classList.toggle("simple");
 
     if (event.target.classList.contains("toggled")) {
       localStorage.setItem("simpleText", "false");
@@ -76,7 +94,9 @@ document.querySelector(".simpleText").addEventListener("click", function (event)
     }
   });
 
-document.querySelector(".open-modal").addEventListener("click", function (event) {
+document
+  .querySelector(".open-modal")
+  .addEventListener("click", function (event) {
     document.querySelector(".access-modal").classList.toggle("show");
   });
 
@@ -84,15 +104,33 @@ document.querySelector(".close").addEventListener("click", function (event) {
   document.querySelector(".access-modal").classList.toggle("show");
 });
 
-document.querySelector(".dyslexiaText").addEventListener("click", function(event) {
+document
+  .querySelector(".dyslexiaText")
+  .addEventListener("click", function (event) {
+    event.target.classList.toggle("toggled");
+    document.querySelector(".title").classList.toggle("dyslexic");
+    document.querySelector(".leaveComment").classList.toggle("dyslexic")
+    document
+      .querySelectorAll(".project")
+      .forEach((item) => item.classList.toggle("dyslexic"));
+
+    if (event.target.classList.contains("toggled")) {
+      localStorage.setItem("dyslexic", "false");
+    } else {
+      localStorage.setItem("dyslexic", "true");
+    }
+  });
+  /* Disqus Comments */
+
+  (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://joelswebprojects.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+
+
+document.querySelector(".show-comments").addEventListener("click", function(event) {
+  document.querySelector("#disqus_thread").classList.toggle("show")
   event.target.classList.toggle("toggled")
-  document.querySelector(".title").classList.toggle("dyslexic")
-  document.querySelectorAll(".project").forEach(item => item.classList.toggle("dyslexic"))
-
-  if (event.target.classList.contains("toggled")) {
-    localStorage.setItem("dyslexic", "false");
-  } else {
-    localStorage.setItem("dyslexic", "true");
-  }
-
 })
