b7655d3bdb665ff763f4ceebbfee71084dc3a29d
joelis10 <joelnod64@gmail.com>
2022-11-01 13:57:15 +0000

    added dyslexia friendly font

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function () {
   const checkAnim = localStorage.getItem("animation");
   const checkShadow = localStorage.getItem("shadow");
   const checkSimpleText = localStorage.getItem("simpleText");
+  const checkIfDyslexic = localStorage.getItem("dyslexic")
 
   if (checkAnim == "off") {
     document.querySelectorAll(".project").forEach((item) => item.classList.add("noAnim"));
@@ -25,6 +26,16 @@ window.onload = function () {
   } else if (checkSimpleText == "false") {
     document.querySelector(".title").classList.remove("simple");
     document.querySelector(".simpleText").classList.add("toggled")
+  }
+
+  if (checkIfDyslexic == "true") {
+    document.querySelector(".title").classList.add("dyslexic");
+    document.querySelectorAll(".project").forEach(item => item.classList.add("dyslexic"));
+    document.querySelector(".dyslexiaText").classList.remove("toggled")
+  } else if (checkSimpleText == "false") {
+    document.querySelector(".title").classList.remove("simple");
+    document.querySelectorAll(".project").forEach(item => item.classList.remove("dyslexic"))
+    document.querySelector(".dyslexiaText").classList.add("toggled")
   }
 };
 
@@ -72,3 +83,16 @@ document.querySelector(".open-modal").addEventListener("click", function (event)
 document.querySelector(".close").addEventListener("click", function (event) {
   document.querySelector(".access-modal").classList.toggle("show");
 });
+
+document.querySelector(".dyslexiaText").addEventListener("click", function(event) {
+  event.target.classList.toggle("toggled")
+  document.querySelector(".title").classList.toggle("dyslexic")
+  document.querySelectorAll(".project").forEach(item => item.classList.toggle("dyslexic"))
+
+  if (event.target.classList.contains("toggled")) {
+    localStorage.setItem("dyslexic", "false");
+  } else {
+    localStorage.setItem("dyslexic", "true");
+  }
+
+})
