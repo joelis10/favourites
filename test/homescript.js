debb86363008b91e15c274209b0b639b39063b12
Joel <joelnod64@gmail.com>
2022-12-27 17:48:14 +0000

    fixed some bugs, added some content

--- test/homescript.js
+++ test/homescript.js
@@ -10,7 +10,12 @@ window.onload = function() {
         document.body.innerHTML = ''
         window.location.replace('/test')
     } else {
-        check.style.display = 'none'
         document.body.style.display = 'flex'
     }
 }
+
+//show comments button
+document.querySelector(".show-comments").addEventListener("click", function(event) {
+    document.querySelector("#disqus_thread").classList.toggle("show")
+    event.target.classList.toggle("toggled")
+})
